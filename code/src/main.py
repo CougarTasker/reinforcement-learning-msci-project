d07c732f2081f1b7617c0a8be9aa4d464bbcfd47
Cougar Tasker <cougartasker@outlook.com>
2023-11-10 17:12:32 +0000

    chore: config improvement
     - added pre-commit hooks and more linting tooling
     - moved virtual environment into repo location for more transparency and to simplify debugging

--- code/src/main.py
+++ code/src/main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+
 class ReinforcementLearningApp(App):
     def build(self):
-        return Label(text='Hello world')
+        return Label(text="Hello world")
+
 
 def main():
     ReinforcementLearningApp().run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
