14f2e8c9c51a7f812d29703cf867a0b42ccd452c
Cougar Tasker <cougartasker@outlook.com>
2023-11-30 07:27:49 +0000

    feat: added an automatic progress mode

--- code/src/view/grid_world_view/grid_world.py
+++ code/src/view/grid_world_view/grid_world.py
@@ -1,6 +1,12 @@
 from typing import Any
 
-from customtkinter import CTkButton, CTkFrame, CTkLabel, CTkOptionMenu
+from customtkinter import (
+    CTkButton,
+    CTkFrame,
+    CTkLabel,
+    CTkOptionMenu,
+    CTkSwitch,
+)
 
 from src.model.learning_system.learning_system import LearningSystem
 
@@ -25,8 +31,11 @@ class GridWorld(CTkFrame):
 
         self.system = system
 
-        self.grid_columnconfigure((0, 1, 2), weight=1)
+        columns = (0, 1, 2, 3)
+        self.grid_columnconfigure(columns, weight=1)
         self.grid_rowconfigure(1, weight=1)
+
+        self.auto = False
 
         self.cumulative_reward: float = 0
         self._reward_label = CTkLabel(self)
@@ -34,7 +43,9 @@ class GridWorld(CTkFrame):
         self.set_reward_text()
 
         self._display = DisplayState(self, self.system.get_current_state())
-        self._display.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        self._display.grid(
+            row=1, column=0, columnspan=len(columns), sticky="nsew"
+        )
 
         self.__setup_controls()
 
@@ -80,20 +91,39 @@ class GridWorld(CTkFrame):
         self.system.set_display_mode(self.display_mode_options[option])
         self._display.set_state(self.system.get_current_state())
 
+    def toggle_auto(self):
+        """When the auto button has been pressed.
+
+        automatically press the next button
+        """
+        self.auto = not self.auto
+        self._next_button.configure(state="disabled" if self.auto else "normal")
+        self.__run_auto()
+
     def __setup_controls(self):
-        self._reset_button = CTkButton(
-            self, text="reset", command=self.reset_button_pressed
-        )
-        self._reset_button.grid(row=2, column=1)
-
-        self._next_button = CTkButton(
-            self, text="next", command=self.next_button_pressed
-        )
-        self._next_button.grid(row=2, column=2)
-
         self._display_mode = CTkOptionMenu(
             self,
             values=list(self.display_mode_options.keys()),
             command=self.display_mode_changed,
         )
         self._display_mode.grid(row=2, column=0)
+
+        self._reset_button = CTkButton(
+            self, text="reset", command=self.reset_button_pressed
+        )
+        self._reset_button.grid(row=2, column=1)
+
+        self._auto_progress = CTkSwitch(
+            self, text="auto", command=self.toggle_auto
+        )
+        self._auto_progress.grid(row=2, column=2)
+
+        self._next_button = CTkButton(
+            self, text="next", command=self.next_button_pressed
+        )
+        self._next_button.grid(row=2, column=3)
+
+    def __run_auto(self):
+        if self.auto:
+            self.next_button_pressed()
+            self.after_idle(self.__run_auto)
