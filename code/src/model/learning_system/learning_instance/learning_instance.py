621fe66dabc6b382346de3447413afddc1c090ba
Cougar Tasker <cougartasker@outlook.com>
2024-04-04 20:12:49 +0000

    feat: made state consistent when switching between agents

--- code/src/model/learning_system/learning_instance/learning_instance.py
+++ code/src/model/learning_system/learning_instance/learning_instance.py
@@ -1,39 +1,9 @@
-from typing import Optional
-
-from typing_extensions import override
-
 from src.model.learning_system.base_entity_decorator import BaseEntityDecorator
-from src.model.learning_system.top_level_entities.container import (
-    EntityContainer,
-)
 from src.model.transition_information import TransitionInformation
 
 
 class LearningInstance(BaseEntityDecorator):
     """An instance of an agent interacting with the environment."""
-
-    def __init__(self, entities: EntityContainer) -> None:
-        """Create the learning instance.
-
-        Args:
-            entities (EntityContainer): the top level entities used in this
-                learning instance.
-        """
-        super().__init__(entities)
-        self._current_state: Optional[int] = None
-
-    @override
-    def update_entities(self, entities: EntityContainer) -> None:
-        """Update the entities used by this decorator.
-
-        Args:
-            entities (EntityContainer): the new entities to use.
-        """
-        different_agent = self.entities.agent is not entities.agent
-        different_dynamics = self.entities.dynamics is not entities.dynamics
-        if different_agent or different_dynamics:
-            self._current_state = None
-        super().update_entities(entities)
 
     def get_current_state(self) -> int:
         """Get the current state ID.
@@ -41,10 +11,10 @@ class LearningInstance(BaseEntityDecorator):
         Returns:
             int: the current state ID
         """
-        if self._current_state is not None:
-            return self._current_state
-        self._current_state = self.dynamics.initial_state_id()
-        return self._current_state
+        current_state = self.statistics.get_statistics().current_state
+        if current_state is not None:
+            return current_state
+        return self.dynamics.initial_state_id()
 
     def reset_state(self) -> int:
         """Reset the current state to the initial state.
@@ -52,8 +22,9 @@ class LearningInstance(BaseEntityDecorator):
         Returns:
             int: the initial state id
         """
-        self._current_state = self.dynamics.initial_state_id()
-        return self._current_state
+        initial_state_id = self.dynamics.initial_state_id()
+        self.statistics.set_current_state(initial_state_id)
+        return initial_state_id
 
     def perform_action(
         self,
@@ -75,5 +46,4 @@ class LearningInstance(BaseEntityDecorator):
         )
         agent.record_transition(transition)
         self.statistics.record_transition(transition)
-        self._current_state = next_state
         return transition
